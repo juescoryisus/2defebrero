067e76a3bc084df8952490b636dcfc1a0764aaa7
Jesus Eduardo <Jesus Eduardo@LAPTOP-AA69H9T5>
2023-03-08 17:38:42 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,232 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.dtpFechaNacimiento = New System.Windows.Forms.DateTimePicker()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(39, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nombre"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(38, 66)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(79, 25)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Telefono"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(38, 114)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(152, 25)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "CorreoElectronico"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(39, 168)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(174, 25)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Fecha de nacimiento"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(161, 23)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(526, 31)
+        Me.txtNombre.TabIndex = 5
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(161, 66)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(150, 31)
+        Me.TextBox2.TabIndex = 6
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(210, 111)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(401, 31)
+        Me.TextBox3.TabIndex = 7
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(161, 553)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(112, 34)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "Listo" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(337, 553)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(112, 34)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "Finalizar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'dtpFechaNacimiento
+        '
+        Me.dtpFechaNacimiento.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpFechaNacimiento.Location = New System.Drawing.Point(210, 169)
+        Me.dtpFechaNacimiento.Name = "dtpFechaNacimiento"
+        Me.dtpFechaNacimiento.Size = New System.Drawing.Size(370, 31)
+        Me.dtpFechaNacimiento.TabIndex = 13
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 25
+        Me.ListBox1.Location = New System.Drawing.Point(30, 220)
+        Me.ListBox1.MaximumSize = New System.Drawing.Size(2, 2)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(2, 4)
+        Me.ListBox1.TabIndex = 4
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4})
+        Me.DataGridView1.Location = New System.Drawing.Point(64, 220)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 62
+        Me.DataGridView1.RowTemplate.Height = 33
+        Me.DataGridView1.Size = New System.Drawing.Size(666, 302)
+        Me.DataGridView1.TabIndex = 15
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Nombre"
+        Me.Column1.MinimumWidth = 8
+        Me.Column1.Name = "Column1"
+        Me.Column1.ReadOnly = True
+        Me.Column1.Width = 150
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Telefono"
+        Me.Column2.MinimumWidth = 8
+        Me.Column2.Name = "Column2"
+        Me.Column2.ReadOnly = True
+        Me.Column2.Width = 150
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "Correo "
+        Me.Column3.MinimumWidth = 8
+        Me.Column3.Name = "Column3"
+        Me.Column3.Width = 150
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "fecha de nacimiento"
+        Me.Column4.MinimumWidth = 8
+        Me.Column4.Name = "Column4"
+        Me.Column4.Width = 150
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(536, 553)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(112, 34)
+        Me.Button3.TabIndex = 16
+        Me.Button3.Text = "Cerrar"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.ClientSize = New System.Drawing.Size(800, 645)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.dtpFechaNacimiento)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents dtpFechaNacimiento As DateTimePicker
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+End Class
---
+++ Form1.vb
@@ -0,0 +1,71 @@
+﻿Public Class Form1
+
+
+    'DataGridView1.Rows.Add(txtNombre.Text, TextBox2.Text, TextBox3.Text, dtpFechaNacimiento.Value)
+
+    ' txtNombre.Text = ""
+    'txtNombre.Focus()
+
+    'dtpFechaNacimiento.Value = ""
+    'dtpFechaNacimiento.Focus()
+
+    'TextBox2.Text = ""
+    'TextBox2.Focus()
+
+    'TextBox3.Text = ""
+    'TextBox3.Focus()
+
+    Dim Nombre(5) As String  '//En este declaramos las variables 
+    Dim Correo(5) As String  '//En este declaramos las variables 
+    Dim Telefono(5) As String  '//En este declaramos las variables 
+    Dim FechadeNacimiento(5) As DateTime? '//En este declaramos la FECHA DE NACIMIENTO con un DateTime?, y el signo de interrogacion es para mostrar que no hay uno especificado aqui y que tendremos que agregarlo mediante la depuracion en el DateTimePicker
+
+    Dim i As Integer = 0 '// Aqui declaramos una variable para poder utilizarlo como un arreglo
+
+    Public Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click  '//En este boton se mostrara todo lo que escribamos dentro de el 
+
+        '// Este es todo lo que se hara en el primer boton
+
+        Nombre(i) = txtNombre.Text  '//Estas son las variables y sus respectivos textbox
+        Telefono(i) = TextBox2.Text  '//Estas son las variables y sus respectivos textbox
+        Correo(i) = TextBox3.Text  '//Estas son las variables y sus respectivos textbox
+        FechadeNacimiento(i) = dtpFechaNacimiento.Value '//Estas son las variables y sus respectivos textbox
+
+        MsgBox("Arreglo" & (i) & "Registrado") '//Este es el mensaje que se mostrara una vez que allamos agregado 
+
+        If i = 5 Then   '//En este if mostrara el mensaje si se llena
+            MsgBox("Esta lleno")  '//En este if mostrara el mensaje si se llena
+        Else
+            i = i + 1  '//En esta forma se incrementa en 1 hasta llegar al limite escrito (Expresado)
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click '//Este es el boton 2 al que le oprimiremos para mostrar todo lo que este dentro de este
+
+        '// Este es todo lo que se hara en el primer boton
+
+
+        'Dim z As Integer = 0  //Este es una forma de hacer una linea extra, Es decir una vez que agregemos el primer texto y lo guardamos el segundo tendra una repeticion extra y el tercero tendra otra 
+
+
+        'While (z <= (i - 1))  //Este es una forma de hacer una linea extra, Es decir una vez que agregemos el primer texto y lo guardamos el segundo tendra una repeticion extra y el tercero tendra otra 
+
+        DataGridView1.Rows.Add(txtNombre.Text, TextBox2.Text, TextBox3.Text, dtpFechaNacimiento.Value) '//Sirve para ordenas cada texto que escribamos en una sola linea y en las columnas que vayan.
+        'Si haces varios hacia abajo y luego a cada uno le agregas el texto estos seran de forma vertical 
+
+        txtNombre.Text = "" '//Este sirve para que limpie lo que escribimos en pantalla una vez que lo allamos agregado
+        TextBox2.Text = "" '//Este sirve para que limpie lo que escribimos en pantalla una vez  que lo allamos agregado
+        TextBox3.Text = "" '//Este sirve para que limpie lo que escribimos en pantalla una vez  que lo allamos agregado
+        'dtpFechaNacimiento.Value = ""  '//Este no lo utilices ya que si lo haces te marcara un error , pero te funcionara bien 
+
+        ' z += 1 //Este es una forma de hacer una linea extra, Es decir una vez que agregemos el primer texto y lo guardamos el segundo tendra una repeticion extra y el tercero tendra otra 
+        'End While //Este es una forma de hacer una linea extra, Es decir una vez que agregemos el primer texto y lo guardamos el segundo tendra una repeticion extra y el tercero tendra otra 
+
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click  '//En este boton cerramos la depuracion (lo que esta mostrando en pantalla para escribir)
+        End '//En este finaliza todo cerrando la depuracion(la ejecucion)
+    End Sub
+End Class
